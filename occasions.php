0b4902786fddb75c97c2c54df05eea5bdcf459b3
BLO3MPJ3 <75973267+BLO3MPJ3@users.noreply.github.com>
2021-02-19 17:30:20 +0100

    Proefrit aanvraag formulier toegevoegd
    Proefrit formulier gemaakt en toegevoegd. Daarnaast heb ik ook de verschillende foto's van de 2 auto's toegevoegd. Css pagina's voor de main, occasion en proefrit/offerte is ook aangemaakt

--- occasions.php
+++ occasions.php
@@ -1,6 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="csstemplates/occasion.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+    <title>V!st@Cars - Occasions</title>
+</head>
+
 <?php
     require_once ('./includes/connection.inc.php');
-
+   
         $sql = "SELECT * FROM tb_cars";
 
         $stmt = $pdo->prepare($sql);
@@ -9,18 +19,31 @@
 
         //print_r($result);
 
-    $autos = "<table>"; // generate string with HTML, using the retrieved database values
+    foreach($result as $key => $row) {
 
-        foreach($result as $key => $row) {
+            $autotitle = "<div class= 'hoofdtitel'>"; // generate string with HTML, using the retrieved database values
+            $autobutton = "<div class= 'abutton'>";
+            $autosignature = "<div class= 'kenmerk'>";
     
-            $autos .= "<tr><td rowspan='6'></td><td colspan='5' rowspan='2'><div class='td1'>" . $row['merk'] . " " . $row['model'] . "</div></td><td rowspan='6'><div class='td2'>&euro; " . $row['vraagprijs'] . "</div></td></tr>
-                <tr></tr><tr><td>Status: " . $row['status'] . "</td><td>Bouwjaar: " . $row['bouwjaar'] . "</td><td>Kmstand: " . $row['kmstand'] . "</td><td>Kenteken: " . $row['kenteken'] . "</td><td>Kleur: " . $row['kleur'] . "</td></tr>
-                <tr><td>APK: " . $row['apk'] . "</td><td></td><td>Stuurbekrachtiging: " . $row['stuurbekrachtiging'] . "</td><td>Airbag: " . $row['airbag'] . "</td><td>Brandstof: " . $row['brandstof'] . "</td></tr>
-                <tr><td>Gewicht: " . $row['gewicht'] . "</td><td></td><td>Transmissie: " . $row['transmissie'] . "</td><td></td><td></td></tr>
-                <tr><td>Aantal deuren: " . $row['deuren'] . "</td><td>Airco: " . $row['airco'] . "</td><td>ABS: " . $row['abs_optie'] . "</td><td></td><td></td></tr><tr></tr>";
-         }
-    
-    $autos .= "</table>";
-    echo $autos;
-    
+            $autotitle .= $row['merk'] . " " . $row['model'] . " &euro; " . $row['vraagprijs'] . " ";
+            $autobutton .= "<a href='./includes/proefritaanvraag.inc.php?idauto=" . $row['id'] ."'><button>Proefrit aanvragen</button></a>" . " " . "<button type='button'>Offerte aanvragen</button>" . " ";
+            $autosignature .= "Status:" . $row['status'] . " " . "Bouwjaar:" . $row['bouwjaar'] . " " . "KM stand:" . $row['kmstand'] . " " . "Kenteken:" . $row['kenteken'] . " " . "Kleur:" . $row['kleur'] . " " . "APK:" . $row['apk'] . " " . "Stuurbekrachtiging:" . $row['stuurbekrachtiging'] . " " . 
+            "Airbag:" . $row['airbag'] . " " . "Brandstof:" . $row['brandstof'] . " " . "Gewicht:" . $row['gewicht'] . " " . "Transmissie:" . $row['transmissie'] . " " . "Deuren:" . $row['deuren'] . " " . "Airco:" . $row['airco'] . " " . "ABS optie:" . $row['abs_optie'] . " ";
+
+        
+
+    $autotitle .= "</div>";
+    $autobutton .= "</div>";
+    $autosignature .= "</div>";
+
+
+
+    echo $autotitle;
+    echo $autobutton;
+    echo $autosignature;
+
+ }
+
 ?>
+
+
