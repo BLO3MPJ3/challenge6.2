1f7a8c87725c7adbfdade53cd8e31a4dade84145
CraZzyIceeNicK <405965@vistacollege.nl>
2021-03-01 13:46:04 +0100

    contact pagina

---
+++ contact.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="csstemplates/contact.css" type="text/css">  
+  <title>Contact</title>
+</head>
+<body>
+  <section class="location_info">
+
+    <section class="titleCON"><h1 class="title1">Contact Info</h1></section>
+      <section class="contactinfo1">
+        <h3>Algemene Info</h3>
+        Telefoonnummer: 043 1234567 </br>
+        Email: info@vistacars.nl </br>
+      </section>
+
+      <section class="contactinfo2">
+        <h3>Openingstijden</h3>
+        Maandag: 8:00 - 17:00 </br>
+        Dinsdag: 8:00 - 17:00 </br>
+        Woensdag: 8:00 - 17:00 </br>
+        Donderdag: 8:00 - 17:00 </br>
+        Vrijdag: 8:00 - 17:00 </br>
+        Zaterdag: Gesloten </br>
+        Zondag: Gesloten </br>
+      </section>
+
+      <section class="contactinfo3">
+        <h3>Hier komt ?</h3>
+      </section>
+
+      <br><br><br><br>
+
+    <section class="titleCON"><h1 class="title1">Locatie Info</h1></section>
+
+      <section id="IFNCON" class="infoCON">
+        <section class="subtitle">Maastricht</section>
+        <section class="adress1"> 
+          <address>
+            E-mail: <a href = "mailto: abc@example.com">info@vistacars.nl</a><br />
+            Tel: <a href="tel:+3161234567">043 1234567</a>
+          </address>
+        </section>
+        <section class="map">
+          <div style="width: 100%"><iframe width="360" height="360" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=320&amp;height=320&amp;hl=nl&amp;q=Sibemaweg%2020,%206224%20DC,%20Maastricht+(Mijn%20bedrijfsnaam)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+        </section>
+      </section>
+
+      <section id="IFNCON1" class="infoCON">
+        <section class="subtitle">Sittard</section>
+        <section class="adress1"> 
+          <address>
+            E-mail: <a href = "mailto: abc@example.com">info@vistacars.nl</a><br />
+            Tel: <a href="tel:+3161234567">046 1234567</a>
+          </address>
+        </section>
+        <section class="map">
+          <div style="width: 100%"><iframe width="360" height="360" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=320&amp;height=320&amp;hl=nl&amp;q=Arendstraat%2012,%206135%20KT%20Sittard+(Mijn%20bedrijfsnaam)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+        </section>
+      </section>
+
+      <section id="IFNCON" class="infoCON">
+        <section class="subtitle">Heerlen</section>
+          <section class="adress1"> 
+            <address>
+              E-mail: <a href = "mailto: abc@example.com">info@vistacars.nl</a><br />
+              Tel: <a href="tel:+3161234567">045 1234567</a>
+            </address>
+          </section>
+          <section class="map">
+            <div style="width: 100%"><iframe width="360" height="360" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=320&amp;height=320&amp;hl=nl&amp;q=Valkenburgerweg%20148,%206419%20AW%20Heerlen+(Mijn%20bedrijfsnaam)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+          </section>
+      </section>
+
+      <section id="coronamelding">
+        <section class="subtitle">Corona</section>
+          <section class="coronainfo">
+            Dankzij corona hebben we de openingstijden aangepast. </br>
+            Meer te vinden op de <a href="nieuws.php">Nieuws Pagina</a>. </br>
+            Tijdens de corona tijd proberen we zo veel mogelijk te werken aan de nieuwe locaties. </br> </br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+            recusandae alias error harum maxime adipisci amet laborum.
+            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+            quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+            fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+            consequuntur!
+          </section>
+      </section>
+
+      <section class="titlecontactopnemen"><h1 class="title1">Contact Opnemen</h1></section>
+      <section class="contactopnemen">
+        <form action="" enctype="multipart/form-data" method="post">
+          <section class="mailtel">
+            <section class="mailtel_inf">
+              <label for="phone">E-mail*:</label><br />
+              <input type="email" name="mail" placeholder="example@gmail.com" required> 
+            </section>
+            <section class="mailtel_inf">
+              <label for="phone">Telefoonnummer*:</label><br />
+              <input type="tel" name="phone" placeholder="06 12345678">
+            </section>
+        </section>
+
+        <section class="subname">
+          <section class="subname_info">
+            <label for="">Onderwerp*:</label><br />
+            <select>
+              <option value="">Kies onderwerp</option>
+              <option value="klacht">Klacht</option>
+              <option value="feedback">Feedback</option>
+              <option value="vraag">Vraag</option>
+              <option value="proefrit">Proefrit</option>
+              <option value="afspaak maken">Afspraak Maken</option>
+              <option value="andere">Andere</option>
+            </select>
+          </section>
+          <section class="subname_info">
+            <label for="phone">Naam*:</label><br />
+            <input type="text" name="name" placeholder="Voornaam en Achternaam" required>
+          </section>
+        </section>
+
+
+        <section class="adres">
+          <section class="adres_info1">
+            <label for="">Provincie:</label><br />
+            <select name="provincie" id="provincie">
+              <option value="Drenthe">Drenthe</option>
+              <option value="Flevoland">Flevoland</option>
+              <option value="Friesland">Friesland</option>
+              <option value="Gelderland">Gelderland</option>
+              <option value="Groningen">Groningen</option>
+              <option value="nl_limburg" selected>Limburg</option>
+              <option value="Noord-Brabant">Noord-Brabant</option>
+              <option value="Noord-Holland">Noord-Holland</option>
+              <option value="Overijssel">Overijssel</option>
+              <option value="Utrecht">Utrecht</option>
+              <option value="Zeeland">Zeeland</option>
+              <option value="Zuid-Holland">Zuid-Holland</option>
+            </select>
+          </section>
+            <section class="adres_info2">
+              <label for="phone">Adres:</label><br />
+              <input type="text" name="postcode" placeholder="Postcode" required>
+              <input type="text" name="huisnummer" placeholder="Huisnummer" required>
+            </section>
+        </section>
+
+
+        <section class="meeting">
+          <section class="meeting_info">
+            <label for="phone">Datum:</label><br />
+            <input type="date" name="date"><br>
+          </section>
+            <section class="meeting_info">
+              <label for="phone">Tijden:</label><br />
+              <select required> selected
+                <option value=""selected disabled>Kies uw afspraak </option>
+                <option value="TI_1a">08:30 - 10:00</option>
+                <option value="TI_1b">10:00 - 11:30</option>
+                <option value="" disabled></option>
+                <option value="TI_2a">12:00 - 13:30</option>
+                <option value="TI_2b">13:30 - 15:00</option>
+                <option value="" disabled></option>
+                <option value="TI_3a">15:30 - 17:00</option>
+              </select>
+            </section><br /><br /><br />
+        </section>
+        
+        <section class="message">
+          <label for="phone">Bericht:</label><br />
+          <textarea type="text" name="w3review" rows="8" cols="58" placeholder="Bericht(max 400 tekens)" maxlength="400"></textarea>
+        </section><br/>
+          <section class="button"><input type="button" value="Verzenden" onclick="msg()"></section>
+        
+    </form>
+  </section>
+  </section>
+
+</body>
+</html>
